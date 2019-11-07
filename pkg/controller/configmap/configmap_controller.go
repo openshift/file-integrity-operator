dcfa0b1c6b8119ed5f69eafb3a1993e3e4d115ae
Matt Rogers <mrogers@redhat.com>
2019-11-07 12:58:19 -0500

    run fmt on configmap_controller.go

--- pkg/controller/configmap/configmap_controller.go
+++ pkg/controller/configmap/configmap_controller.go
@@ -5,11 +5,11 @@ import (
 	"fmt"
 	"time"
 
-	"k8s.io/apimachinery/pkg/types"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
