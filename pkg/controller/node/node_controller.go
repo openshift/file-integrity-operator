d2ce388f4fdf6c6cc14466944eb6e1df7a461392
Jakub Hrozek <jhrozek@redhat.com>
2020-09-30 15:01:15 +0200

    nodectrl: Remove unused constants
    These constants were unused since commit
    96233c96dee4abbecdd6c1684ac9333a1e2772ae

--- pkg/controller/node/node_controller.go
+++ pkg/controller/node/node_controller.go
@@ -24,11 +24,6 @@ import (
 
 var log = logf.Log.WithName("controller_node")
 
-const (
-	addHoldOffScriptCMName    = "addholdoff"
-	removeHoldOffScriptCMName = "rmholdoff"
-)
-
 // Add creates a new Node Controller and adds it to the Manager. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
 func Add(mgr manager.Manager) error {
