067a3b3a8baafa0e7f5228dd3d5c98cddf2a6b6f
Carlo Cassanelli <cassatek@gmail.com>
2024-03-04 10:46:27 +0100

    Exclude multus certs from scanning

--- pkg/controller/fileintegrity/config_defaults.go
+++ pkg/controller/fileintegrity/config_defaults.go
@@ -52,6 +52,7 @@ CONTENT_EX = sha512+ftype+p+u+g+n+acl+selinux+xattrs
 !/hostroot/etc/machine-config-daemon/node-annotation.json*
 !/hostroot/etc/pki/ca-trust/extracted/java/cacerts$
 !/hostroot/etc/cvo/updatepayloads
+!/hostroot/etc/cni/multus/certs/*
 
 # Catch everything else in /etc
 /hostroot/etc/    CONTENT_EX`
